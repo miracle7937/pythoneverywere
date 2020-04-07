a084b032a992454460b8921c1ef2b61c9316f9d2
chukwude <ebukamiracle35@gmail.com>
2020-04-07 23:54:31 +0100

    make it better

--- EHealth/settings.py
+++ EHealth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '65&jds6p#br5yu=ov^juxnh6k56ae!djhgu-blsxvc+ryxqq-e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythontestappeheath.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
